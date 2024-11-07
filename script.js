975d91ebcb05fa0d9b6f752616a31f834db3dc09
LawSibisi <lawrencesibisi99@gmail.com>
2024-11-07 10:00:34 +0200

    update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
-    } else{
+    } 
+    else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
     }
 }
